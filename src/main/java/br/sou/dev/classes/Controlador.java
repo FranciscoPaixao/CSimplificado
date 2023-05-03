c68477ce1082a7e3a33c16a77b0fa67ca269c2f2
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-05-03 10:04:47 -0300

    Update Controlador.java

--- src/main/java/br/sou/dev/classes/Controlador.java
+++ src/main/java/br/sou/dev/classes/Controlador.java
@@ -4,10 +4,34 @@
  */
 package br.sou.dev.classes;
 
+import br.dev.sou.testeantlr.CSimplificadoLexer;
+import java.io.IOException;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+
 /**
  *
  * @author Francisco
  */
 public class Controlador {
+    private CharStream codigoFonte;
+    private String caminhoArquivo;
+    private CSimplificadoLexer lexer;
     
+    public Controlador(){
+        this.caminhoArquivo = null;
+        this.codigoFonte = null;
+        this.lexer = null;
+    }
+    
+    public void LerArquivo(String caminhoArquivo) throws IOException{
+        this.caminhoArquivo = caminhoArquivo;
+        codigoFonte = CharStreams.fromFileName(caminhoArquivo);
+    }
+    public void AtualizarArquivo() throws IOException{
+        LerArquivo(caminhoArquivo);
+    }
+    public void FazerAnaliseLexica(){
+         this.lexer = new CSimplificadoLexer(codigoFonte);
+    }
 }
